109f177798bbeb3f534e50d70a71f94a04c40b9a
Mona Lisa <s3817719@rmit.edu.vn>
2021-12-02 16:33:50 +0700

    add payment method and promotion

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -8,33 +8,37 @@ import java.util.Scanner;
 
 public class Main {
 
+    //define variables
+    private static int limitedBudget = 50000;
+    private static double winRateIncrease = 0.5;
+    private static double percentageWinRate = 0.1;
+
+    //define formatter for format Date
+    private static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    private static Date date = new Date();
+    private static String stringDate = formatter.format(date);
+    private static Date timeMachineRun = null;
+
+
     public static void main(String[] args) {
-        String qh = "01/12/2021";
-        Date date1 = null;
+        //set the date when machine start
         try {
-            date1 = new SimpleDateFormat("dd/MM/yyyy").parse(qh);
+            timeMachineRun = formatter.parse(stringDate);
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        Date date2 = new Date();
-        String huy = formatter.format(date2);
-        Date date3 = null;
-        try {
-            date3 = new SimpleDateFormat("dd/MM/yyyy").parse(huy);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        System.out.println(isSameDay(date1, date3));
+        //call menu function
         menu();
     }
 
     public static void menu(){
+        // create Scanner to user input
         Scanner sc = new Scanner(System.in);
 
+        // total notes
         int total = 0;
+        //loop until they exit program
         while(true) {
             boolean run = true;
             System.out.println("------------------------");
@@ -48,6 +52,8 @@ public class Main {
             System.out.println("5. 200.000");
             System.out.println("6. Exit");
             System.out.println("------------------------");
+
+            //loop until choose exit or do not continue input notes (go to choose product)
             while (run) {
                 System.out.print("Your choice: ");
                 String choice = sc.next();
@@ -61,45 +67,60 @@ public class Main {
                     total += 100000;
                 } else if (choice.equals("5")) {
                     total += 200000;
-                }else if (choice.equals("6")) {
+                }else if (choice.equals("6")) { // Exit program
                     return;
                 }else {
+                    //Wrong input, input again
                     System.out.println("Wrong input");
                 }
                 while(true){
+                    //Ask user to continue adding notes or go to next step (choose product)
                     System.out.println("------------------------");
-                    System.out.println("Do you wanna continue input notes (y/n): ");
+                    System.out.println("Do you wanna continue input notes or go to choose product (y/n): ");
                     System.out.println("------------------------");
                     System.out.print("Your choice: ");
                     String asking = sc.next();
                     if (asking.equals("y") || asking.equals("Y")) {
+                        // if yes, run = false to stop choice and run loop out of it
                         run = false;
                         break;
                     } else if (asking.equals("n") || asking.equals("N")) {
-
+                        // if no, go to choose product
                         chooseProduct(total);
                         return;
                     } else {
+                        //if wrong, user can input again
                         System.out.println("Wrong input");
                     }
                 }
-
             }
         }
     }
 
     public static void chooseProduct(int currentUserBudget){
         Scanner sc = new Scanner(System.in);
+        //Create hashmap to store cart of user
         HashMap<String, Integer> cart = new HashMap<>();
+        // total payment
         int total = 0;
+        //initialize values
         cart.put("Coke", 0);
         cart.put("Pepsi", 0);
         cart.put("Soda", 0);
         System.out.println("------------------------");
 
         while(true) {
+            //show current budget and cart of user
             System.out.println("------------------------");
             System.out.println("Your total budget: " + currentUserBudget);
+            System.out.println("------------------------");
+            System.out.println("Your Cart:");
+            for (String key: cart.keySet()) {
+                if (cart.get(key) > 0){
+                    System.out.println("    " + key + ": " + cart.get(key));
+                }
+            }
+            System.out.println("------------------------");
             System.out.println("Please input your product: ");
             System.out.println("1. Coke (10.000)");
             System.out.println("2. Pepsi (10.000)");
@@ -111,9 +132,13 @@ public class Main {
                 System.out.print("Your choice: ");
                 String choice = sc.next();
                 if (choice.equals("1")) {
+                    //check budget is enough or not
                     if (currentUserBudget >= 10000){
+                        // increase the quantity
                         cart.put("Coke", cart.get("Coke") + 1);
+                        // minus current budget of user
                         currentUserBudget -= 10000;
+                        // increase payment
                         total += 10000;
                     }else{
                         System.out.println("------------------------");
@@ -145,17 +170,21 @@ public class Main {
                         break;
                     }
                 }else if (choice.equals("4")) {
+                    //exit program
                     return;
                 }else {
                     System.out.println("Wrong input");
                 }
+
+
                 if (currentUserBudget < 10000){
+                    // check if budget is not enough to continue buy, call payment method
                     System.out.println("Your money do not enough for buy other products");
 
-                    System.out.println(cart + " "+ total +" " + currentUserBudget);
                     paymentMethod(cart, total, currentUserBudget);
                     return;
                 }else{
+                    //if enough, user can choose another product
                     System.out.println("------------------------");
                     System.out.println("Do you wanna continue buy product (y/n): ");
                     System.out.println("------------------------");
@@ -165,7 +194,7 @@ public class Main {
                         run = false;
                         break;
                     } else if (asking.equals("n") || asking.equals("N")) {
-                        System.out.println(cart + " "+ total +" " + currentUserBudget);
+                        //if no, call payment method
                         paymentMethod(cart, total, currentUserBudget);
                         return;
                     } else {
@@ -182,6 +211,16 @@ public class Main {
 
         while(true) {
             boolean run = true;
+            //show all cart, user's budget, total payment
+            System.out.println("------------------------");
+            System.out.println("Your Cart:");
+            for (String key: cart.keySet()) {
+                if (cart.get(key) > 0){
+                    System.out.println("    " + key + ": " + cart.get(key));
+                }
+            }
+            System.out.println("Your total payment: " + total);
+            System.out.println("Your budget: " + currentUserBudget);
             System.out.println("------------------------");
             System.out.println("Do you wanna buy or refund: ");
             System.out.println("1. Yes");
@@ -191,7 +230,71 @@ public class Main {
                 System.out.print("Your choice: ");
                 String choice = sc.next();
                 if (choice.equals("1")) {
+                    // take date of user buy pay the bill
+                    Date date2 = new Date();
+                    // format Date to String to get dd/mm/yyyy (do not get time)
+                    String stringDate1 = formatter.format(date2);
+                    Date timeOfPayment = null;
+                    try {
+                        // parse String to Date
+                        timeOfPayment = formatter.parse(stringDate1);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Your Cart:");
+                    for (String key: cart.keySet()) {
+                        if (cart.get(key) > 0){
+                            System.out.println("    " + key + ": " + cart.get(key));
+                        }
+                    }
+                    System.out.println("Your total payment: " + total);
+                    System.out.println("Your budget: " + currentUserBudget);
 
+                    //if difference of two dates is more than 0
+                    if (differenceOfTwoDates(timeOfPayment, timeMachineRun) > 0){
+                        if (limitedBudget > 0){
+                            // if limited budget of bonus is more than 0, do not reach so increase the chance
+                            // the increase chance is calculating by using formula : percent of win rate * (1 + win rate increase)
+                            percentageWinRate = percentageWinRate * Math.pow((1+ winRateIncrease), differenceOfTwoDates(timeOfPayment, timeMachineRun));
+                        }else{
+                            percentageWinRate = 0.1;
+                        }
+                        limitedBudget = 50000;
+                        timeMachineRun = timeOfPayment;
+                    }
+
+
+                    boolean checkBonus = false;
+                    for (String key: cart.keySet()){
+                        int timesBonus = cart.get(key);
+                        for(int i  = 0 ; i < timesBonus/3 ; i++) {
+                            // check if successfully get a free product
+                            if (promotion(percentageWinRate)) {
+                                if (limitedBudget - (key.equals("Soda") ? 20000 : 10000) >= 0){
+                                    checkBonus = true;
+                                    cart.put(key, cart.get(key) + 1); // add 1 free product
+                                    // decrease limitedBudget if user get a free product
+                                    limitedBudget -= key.equals("Soda") ? 20000 : 10000;
+                                    System.out.println("Congratulation! You receive 1 free " + key);
+
+                                }
+                            }
+                        }
+                    }
+                    if (checkBonus){
+                        System.out.println("Now! Your Cart:");
+                        for (String key: cart.keySet()) {
+                            if (cart.get(key) > 0){
+                                System.out.println("    " + key + ": " + cart.get(key));
+                            }
+                        }
+                        System.out.println("Your total payment: " + total);
+                        System.out.println("Your budget: " + currentUserBudget);
+                    }else{
+                        System.out.println("Sorry you do not have any bonus");
+                    }
+
+                    return;
                 }else if (choice.equals("2")) {
                     int refund = total + currentUserBudget;
                     System.out.println("Your refund is " + refund);
@@ -203,14 +306,17 @@ public class Main {
         }
     }
 
-    public static boolean isSameDay(Date date1, Date date2) {
+    public static long differenceOfTwoDates(Date date1, Date date2) {
 
         // Strip out the time part of each date.
         long julianDayNumber1 = date1.getTime();
         long julianDayNumber2 = date2.getTime();
-        System.out.println(julianDayNumber1);
-        System.out.println(julianDayNumber2);
+        long distanceOfTwoDate = julianDayNumber1 - julianDayNumber2;
         // If they now are equal then it is the same day.
-        return julianDayNumber1 == julianDayNumber2;
+        return distanceOfTwoDate/(60*60*24*1000);
+    }
+
+    public static boolean promotion(double percentageWinRate){
+        return Math.random() <= percentageWinRate;
     }
 }
